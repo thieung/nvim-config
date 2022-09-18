282dc719e630e20615b87b576de5b82c2080e3fb
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-09-18 21:38:48 +0700

    (mini.nvim) update config

--- lua/config/plugin/mini/setup.lua
+++ lua/config/plugin/mini/setup.lua
@@ -1,35 +1,40 @@
-require("mini.bufremove").setup({})
-require("mini.comment").setup({})
-require("mini.trailspace").setup({})
-require("mini.jump").setup({})
-require("mini.surround").setup({})
-require("mini.pairs").setup({})
+require("mini.sessions").setup({ directory = "~/.config/nvim/misc/sessions" })
+require("mini.starter").setup()
+vim.cmd([[autocmd User MiniStarterOpened setlocal fillchars=eob:\ ]])
 
-require("mini.jump2d").setup({
-  mappings = {
-    start_jumping = "S",
-  },
-})
+vim.defer_fn(function()
+  require("mini.bufremove").setup()
+  require("mini.comment").setup()
+  require("mini.trailspace").setup()
+  require("mini.jump").setup()
 
-require("mini.indentscope").setup({
-  draw = {
-    delay = 0,
-    animation = require("mini.indentscope").gen_animation("none"),
-  },
-  mappings = {
-    object_scope = "",
-    object_scope_with_border = "",
-    goto_top = "",
-    goto_bottom = "",
-  },
-  options = { indent_at_cursor = false },
-  symbol = "▏",
-})
+  require("mini.surround").setup({ search_method = "cover_or_next" })
 
-local gen_spec = require("mini.ai").gen_spec
-require("mini.ai").setup({
-  custom_textobjects = {
-    ["*"] = gen_spec.pair("*", "*", { type = "greedy" }),
-    ["_"] = gen_spec.pair("_", "_", { type = "greedy" }),
-  },
-})
+  require("mini.pairs").setup({
+    modes = { insert = true, command = true, terminal = true },
+  })
+
+  require("mini.jump2d").setup({
+    mappings = {
+      start_jumping = "S",
+    },
+  })
+
+  require("mini.indentscope").setup({
+    draw = {
+      delay = 0,
+      animation = require("mini.indentscope").gen_animation("none"),
+    },
+    options = { indent_at_cursor = false },
+    symbol = "▏",
+  })
+
+  require("mini.ai").setup({
+    custom_textojects = {
+      F = require("mini.ai").gen_spec.treesitter({
+        a = "@function.outer",
+        i = "@function.inner",
+      }),
+    },
+  })
+end, 0)
