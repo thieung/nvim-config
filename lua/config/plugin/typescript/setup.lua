7d4287532af844d52f0bca608bf76c4a271b15db
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-12-20 23:28:28 +0700

    (config.plugin.typescript) specify root_dir

--- lua/config/plugin/typescript/setup.lua
+++ lua/config/plugin/typescript/setup.lua
@@ -3,6 +3,15 @@ if not client then
   return false
 end
 
+-- This prevents `tsserver` from running on non Node.js projects
+-- (e.g. deno) by removing `.git` as `root_dir` and specifying it with
+-- common Node.js project file such as `package.json`.
+client.root_dir = require("lspconfig.util").root_pattern(
+  "package.json",
+  "tsconfig.json",
+  "jsconfig.json"
+)
+
 require("typescript").setup({
   disable_formatting = true,
   server = client,
