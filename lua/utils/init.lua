3e50073a52d1a4520f530b6cb6d62f4af7046730
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-12-23 12:41:13 +0700

    (utils.lazy_load) add lazy load function

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -6,5 +6,6 @@ M.Keymap = require("utils.keymap")
 M.Highlight = require("utils.highlight")
 M.StatusLine = require("utils.statusline")
 M.General = require("utils.general")
+M.Lazyload = require("utils.lazy_load")
 
 return M
