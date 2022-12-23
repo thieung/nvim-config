c8bc1d967170687b314bd2256f7a47a2b4a4103a
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-12-23 12:40:20 +0700

    (config.plugin.neo-tree) add source selectors and update neo-tree config

--- lua/config/plugin/neo-tree/setup.lua
+++ lua/config/plugin/neo-tree/setup.lua
@@ -3,12 +3,49 @@ if not status_ok then
   return
 end
 
-vim.g.neo_tree_remove_legacy_commands = true
+local renderer = require("neo-tree.ui.renderer")
+-- ascend to the parent or close it
+local function float(state)
+  local node = state.tree:get_node()
+  if
+    (node.type == "directory" or node:has_children()) and node:is_expanded()
+  then
+    state.commands.toggle_node(state)
+  else
+    renderer.focus_node(state, node:get_parent_id())
+  end
+end
+
+-- toggle a node open or descend to it's first child
+local function dive(state)
+  local node = state.tree:get_node()
+  if node.type == "directory" or node:has_children() then
+    if not node:is_expanded() then
+      if node.type == "directory" then
+        require("neo-tree.sources.filesystem").toggle_directory(state, node)
+      else
+        state.commands.toggle_node(state)
+      end
+    else
+      renderer.focus_node(state, node:get_child_ids()[1])
+    end
+  end
+end
 
 neotree.setup({
   close_if_last_window = true,
   popup_border_style = "rounded",
   enable_diagnostics = false,
+  source_selector = {
+    winbar = true,
+    content_layout = "center",
+    tab_labels = {
+      filesystem = " File",
+      buffers = " Bufs",
+      git_status = " Git",
+      diagnostics = " Diagnostic",
+    },
+  },
   default_component_configs = {
     indent = {
       padding = 0,
@@ -36,26 +73,40 @@ neotree.setup({
   },
   window = {
     position = "float",
-    width = 25,
+    width = 40,
     mappings = {
-      ["o"] = "open",
+      ["<space>"] = {
+        "toggle_node",
+        nowait = true,
+      },
+      o = "open",
+      ["h"] = float,
+      ["l"] = dive,
+      ["c"] = {
+        "copy",
+        config = {
+          show_path = "relative", -- "none", "relative", "absolute"
+        },
+      },
+      ["Y"] = "copy_file_name",
+      ["<C-y>"] = "copy_file_path",
     },
   },
   filesystem = {
-    filtered_items = {
-      visible = false,
-      hide_dotfiles = true,
-      hide_gitignored = false,
-      hide_by_name = {
-        ".DS_Store",
-        "thumbs.db",
-        "node_modules",
-        "__pycache__",
-      },
-    },
     follow_current_file = true,
     hijack_netrw_behavior = "open_current",
     use_libuv_file_watcher = true,
+    commands = {
+      copy_file_name = function(state)
+        local node = state.tree:get_node()
+        vim.fn.setreg("+", node.name, "c")
+      end,
+      copy_file_path = function(state)
+        local node = state.tree:get_node()
+        local relative_path = node.path:sub(#state.path + 2)
+        vim.fn.setreg("+", relative_path, "c")
+      end,
+    },
   },
   git_status = {
     window = {
